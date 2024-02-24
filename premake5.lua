a170f5331de07ed6a11a7e90ecad59ebc8a3a00d
Tepid Angler <32462088+Tepidangler@users.noreply.github.com>
2024-02-24 17:08:32 -0500

    Add files via upload

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,6 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
-	staticruntime "off"
 	warnings "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
@@ -31,6 +30,7 @@ project "GLFW"
 		pic "On"
 
 		systemversion "latest"
+		staticruntime "On"
 		
 		files
 		{
@@ -53,30 +53,9 @@ project "GLFW"
 			"_GLFW_X11"
 		}
 
-	filter "system:macosx"
-		pic "On"
-
-		files
-		{
-			"src/cocoa_init.m",
-			"src/cocoa_monitor.m",
-			"src/cocoa_window.m",
-			"src/cocoa_joystick.m",
-			"src/cocoa_time.c",
-			"src/nsgl_context.m",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/osmesa_context.c",
-			"src/egl_context.c"
-		}
-
-		defines
-		{
-			"_GLFW_COCOA"
-		}
-
 	filter "system:windows"
 		systemversion "latest"
+		staticruntime "On"
 
 		files
 		{
